0ef8903a821a53824d20f18fb408a5f8fecc015d
Andres <andresperlo5@gmail.com>
2025-01-17 10:06:41 -0300

    se eliminan comentarios y consoles

--- src/helpers/clientAxios.jsx
+++ src/helpers/clientAxios.jsx
@@ -6,18 +6,15 @@ const clientAxios = axios.create({
 
 clientAxios.interceptors.request.use(
   (config) => {
-    // Obtén el token de sessionStorage cada vez que se haga una solicitud
     const token = JSON.parse(sessionStorage.getItem("token"));
 
     if (token) {
-      // Agrega el token al header Authorization
       config.headers.auth = `Bearer ${token}`;
     }
 
     return config;
   },
   (error) => {
-    // Maneja el error
     return Promise.reject(error);
   }
 );
